857c9025160f0ed5dcfb658e66b72bdde0fadfa7
salma-c <92518434+salma-c@users.noreply.github.com>
2022-01-19 14:34:27 +0100

    update

--- footre_desktop.js
+++ footre_desktop.js
@@ -1,30 +1,31 @@
 const template = document.createElement("template");
 template.innerHTML = `
-<footer style="margin-top:190px";>
- <div class="DROITE">
-<div class="phrase">
+<footer>
+<div class="banner">
+<div class="bano">
   <i class="bx bx-map"></i>
   <h4>497 SAFI, CA 95673</h4>
 </div>
-<div class="phrase">
+<div class="bano">
   <i class="bx bx-mobile"></i>
   <h4>+44 345 678 903</h4>
 </div>
-<div class="phrase">
+<div class="bano">
   <i class="bx bxl-gmail"></i>
   <h4>contact@YouCodeTravel.com</h4>
 </div>
 </div>
-<div class="CENTER">
-<h3>Subscribe to our newsletter</h3>
-<div class="button_mail">
-  <input type="email" class="email" placeholder="Email Address" />
-  <button class="ok">OK</button>
+<div class="center">
+<h4 class="h4">Subscribe to our newsletter</h3>
+<div class="butona">
+  <input type="email" class="emaill" placeholder="Email Address" />
+  <button class="okk">OK</button>
 </div>
 </div>
-<div class="GAUCHE">
-<img src="logo.png" class="logo_footer" alt="" />
+<div class="banner2">
+<img src="logo.png" class="logo_" alt="" />
 </div>
+
 </footer>
 `;
 class footre_descktop extends HTMLElement {
